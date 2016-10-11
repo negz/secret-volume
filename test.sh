361525c54698ce09dbf50ad3ba5f781957e72452
Nic Cope <negz@spotify.com>
2016-10-10 21:46:07 -0400

    Run integration tests in Travis

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -e
 echo "" > coverage.txt
 
 for d in $(glide novendor); do
-    go test -v -tags debug -race -coverprofile=profile.out $d
+    go test -v -tags 'debug integration' -race -coverprofile=profile.out $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
